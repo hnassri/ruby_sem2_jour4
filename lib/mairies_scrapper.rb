44109ebbfaaa2b487a06a9fe5e42fac9eec53453
CocoThePimp <jonathan.lutz@gmx.fr>
2019-01-17 23:46:42 +0100

    ajout des commentaires dans mairies

--- lib/mairies_scrapper.rb
+++ lib/mairies_scrapper.rb
@@ -1,6 +1,3 @@
-
-
-
 require 'rubygems'
 require 'nokogiri'  
 require 'open-uri'
@@ -10,7 +7,7 @@ require 'open-uri'
 
 
 
-# -------- Obtenir l'url de la mairie ----------
+# -------- Fonction récup url des mairies ----------
 def get_townhall_urls(city)
 page = Nokogiri::HTML(open('https://www.annuaire-des-mairies.com/95/' + city.split.join('-'))) #change les espaces par des -
 return page
@@ -20,7 +17,7 @@ end
 
 
 
-# -------- Récupérer email ----------
+# --------  Fonction récup des emails  ----------
 def get_townhall_email(townhall_url)
     email = townhall_url.xpath ('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]')
     return email.text 
@@ -30,7 +27,7 @@ end
 
 
 
-# -------- Créer un tableau avec les noms de ville ----------
+# -------- Extraction des noms de ville ----------
 
     page = Nokogiri::HTML(open('http://annuaire-des-mairies.com/val-d-oise.html'))
     
@@ -39,29 +36,29 @@ end
 
 
 
-    # Arrays --
+# -------- Var ---------
     tab_nom_ville = []
     arr_email = []
-    # --------
+    
 
 
-
+# -------- Créer un tableau avec les noms de ville ----------
     nom_ville.each do |name| #
         tab_nom_ville << name.text.downcase #Tableau où se trouve les noms de ville en minuscule
     end
  
 
-
+# -------- Créer un tableau avec les emails de ville ----------
 tab_nom_ville.each do |city|
 
-    get_townhall_urls(city)
-    townhall_url = get_townhall_urls(city) #l'url de la ville = get url de la ville
-    get_townhall_email(townhall_url)
-    if get_townhall_email(townhall_url) =~ /@/
+    get_townhall_urls(city) #fonction utilisant les noms de ville du tableau pr récup l'url
+    townhall_url = get_townhall_urls(city) #l'url de la ville = fonction get url de la ville
+    get_townhall_email(townhall_url) #fonction utilisant l'url pour acquérir l'email
+    if get_townhall_email(townhall_url) =~ /@/ #trouve un @
         arr_email << get_townhall_email(townhall_url)
         
     else
-        arr_email << "bidon@gmail.com" #Remplace les endroits ou il n'y a pas d'email par bidon@gmail.com
+        arr_email << "bidon@gmail.com" #Remplace les endroits où il n'y a pas d'email par bidon@gmail.com
     end
 
 
@@ -81,13 +78,6 @@ hash_come.each do |key, value|
 end
 
 puts array
-
-
-
-
-
-
-
 
 
 
--- spec/mairies_scrapper_spec.rb
+++ spec/mairies_scrapper_spec.rb
@@ -1,15 +1,41 @@
 require_relative '../lib/mairies_scrapper'
 
+# Methode 1
 describe "the email scrapper" do
     it "the type returned is an array" do
-        expect(get_townhall_email(townhall_url).class).to eq(Array)
+        expect(hash_final.class).to eq(Array)
     end
     # it "the returns something" do
     #     expect(get_townhall_urls("avernes")).not_to be_nil
     #   end
-    it "the returns something" do
-        expect(get_townhall_email(townhall_url)).not_to be_nil
-    end
+    # it "the returns something" do
+    #     expect(get_townhall_email(townhall_url)).not_to be_nil
+    # end
   end
 
 
+
+
+  # Methode 2
+#   describe "the procedure to recover emails and city names" do
+#     describe "the function array_name_town" do
+#       it "the type returned is an array" do
+#         expect(array_name_town.class).to eq(Array)
+#       end
+#       it "the returns something" do
+#         expect(array_name_town(avernes)).not_to be_nil
+#       end
+#     end
+    
+#     describe "the function get_townhall_email" do
+#       it "the type returned is a string" do
+#         expect(get_townhall_email("https://www.annuaire-des-mairies.com/95/avernes.html").class).to eq(String)
+#       end
+#       it "the returns something" do
+#         expect(get_townhall_email("https://www.annuaire-des-mairies.com/95/avernes.html")).not_to be_nil
+#       end
+#       it "the value returned from the city Avernes is " do
+#         expect(get_townhall_email("https://www.annuaire-des-mairies.com/95/avernes.html")).to eq("mairie.avernes@orange.fr")
+#       end
+#     end
+#   end
